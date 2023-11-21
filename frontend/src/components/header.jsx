027437906d6d6551fb38b84c6002867fc05ec7f4
YukiP7 <gargyukti112@gmail.com>
2023-11-21 23:05:14 +0530

    Change scripts

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -62,7 +62,7 @@ export default function Header(){
                 </NavLink>
             </li>
             <li>
-                <NavLink to="/"
+                <NavLink to="#"
                     className={({isActive}) =>
                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-blue-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-700 lg:p-0`
                     }
@@ -71,7 +71,7 @@ export default function Header(){
                 </NavLink>
             </li>
             <li>
-                <NavLink to="/"
+                <NavLink to="#"
                     className={({isActive}) =>
                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-blue-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-700 lg:p-0`
                     }
@@ -80,7 +80,7 @@ export default function Header(){
                 </NavLink>
             </li>
             <li>
-                <NavLink to="/"
+                <NavLink to="#"
                     className={({isActive}) =>
                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-blue-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-700 lg:p-0`
                     }
