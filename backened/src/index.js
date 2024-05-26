4e2d3d0d2b6070484339423e16764bd1075b3efa
YukiP7 <gargyukti112@gmail.com>
2024-05-26 12:01:43 +0530

    Changes

--- backened/src/index.js
+++ backened/src/index.js
@@ -5,14 +5,6 @@ import connect_DB from "../src/db/index.js"
 import express from "express" 
 
 const app = express() 
-const router = express.Router();
-
-// Your existing routes and middleware
-router.get('/your-route', (req, res) => {
-  res.json({ message: 'Hello from Netlify Functions' });
-});
-
-app.use('/.netlify/functions/backend', router);
 
 connect_DB()
 .then(() => {
@@ -23,5 +15,3 @@ connect_DB()
 .catch((err) => {
     console.log("MongoDB connection FAILED!! " , err);
 })
-
-export const handler = serverless(app);
